9b096887f9267bc9a50a5bfe36db0f7616c9d5bc
Rémi Rigal <remi.rigal@ensta-bretagne.org>
2017-10-12 17:56:28 +0200

    Map Integration

--- public/js/map.js
+++ public/js/map.js
@@ -1,14 +1,14 @@
 var long, lat, mpp, zoom, scale, size, taillemap, kmpd, deltalong, deltalat;
 var longmin, longmax, latmin, latmax, longpp, latpp;
-var svg;
-var sizeaffichageimage = 500;
+var svg, globalMap;
+var sizeaffichageimage = 1280;
 var circle;
 
 long = -3.020906;                                                   //longitude du centre de l'image, à récupérer plus tard automatiquement via url api google
 lat = 48.2040407;                                                   //idem latitude
 zoom = 15;                                                          //idem zoom
 scale = 2;
-size=640;
+size = 640;
 mpp = 156543.03392 * (Math.cos(lat * Math.PI / 180) / (scale * Math.pow(2, zoom)));    //metre par pixel de l'image
 taillemap = mpp * scale * size;                                                      //taille de la carte en metre
 kmpd = (40075.017 / 360) * Math.cos(lat * Math.PI / 180);                           //km par degré en fonction de la latitude
@@ -26,28 +26,31 @@ function updatePosition(latitude, longitude, remove){
         circle.remove();
     }
     var difflong, nbrpixlong, difflat, nbrpixlat;
+    var wFrame, hFrame;
+    wFrame = globalMap.width();
+    hFrame = globalMap.height();
     
     difflong = longitude-longmin;
     nbrpixlong = Math.round(difflong/longpp);
     difflat = Math.abs(latitude-latmax);
     nbrpixlat = Math.round(difflat/latpp);
 
-    console.log(nbrpixlat, nbrpixlong);
+    globalMap.css("background-position", "-" + (nbrpixlong - (wFrame/2)) + "px -" + (nbrpixlat - (hFrame/2)) + "px");
 
     circle = svg.append("circle")
-        .attr("cx", nbrpixlong)
-        .attr("cy", nbrpixlat)
+        .attr("cx", wFrame/2)
+        .attr("cy", hFrame/2)
         .attr("r", 3)
-        .attr("fill", "red");
+        .attr("fill", "#fab548");
 }
 
 $(document).ready(function() {
+    globalMap = $('#global_map');
     svg = d3.select("#global_map")
         .append("svg")
         .attr("width", 500)
         .attr("height", 500)
         .attr("id", "svg_map");
-    $('#svg_map').html('<image xlink:href="images/mapguerledanglobale.png" height="500px" width="500px"></image>');
 
-    //updatePosition(48.199040, -3.015805);
+    updatePosition(48.199040, -3.015805, true);
 });
