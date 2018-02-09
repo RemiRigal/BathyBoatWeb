e6daf7348157e8426f9410218ddb6114ec6b3382
Rémi Rigal <remi.rigal@ensta-bretagne.org>
2018-02-09 10:18:25 +0100

    Start mission selection

--- public/js/mission_selection.js
+++ public/js/mission_selection.js
@@ -1,3 +1,8 @@
+var addMissionButton, missionList;
+
+var missions = [];
+var missionId = 0;
+
 
 function addMission() {
     $('#add_mission').hide();
@@ -30,3 +35,40 @@ function selectTypeWay() {
     cleanAll();
 }
 
+function Mission(missionId) {
+
+    this.setType = function() {
+
+    };
+
+    this.initHtml = function() {
+        this.htmlElement.html('<div>' +
+            '<div class="pull-left">' + this.name + '</div>' +
+            '<span id="delete_mission_' + this.id + '" class="pull-right glyphicon glyphicon-remove"></span>' +
+            '</div>');
+        $('#delete_mission_' + this.id).on('click', {element: this.htmlElement}, function(event) {
+            event.data.element.remove();
+        });
+    };
+
+    this.id = missionId;
+    this.name = 'Mission' + missionId;
+    this.type = 'Waypoints';
+    this.waypoints = [];
+
+    this.htmlElement = $('#mission_' + missionId);
+    this.initHtml();
+}
+
+function onAddMissionClicked() {
+    missionId++;
+    $('<div class="row" id="mission_' + missionId + '"  style="padding-left: 15px; padding-right: 15px"></div>').appendTo(missionList);
+    missions.push(new Mission(missionId));
+}
+
+
+$(document).ready(function() {
+    addMissionButton = $('#add_mission');
+    missionList = $('#missions_list');
+    addMissionButton.on('click', onAddMissionClicked);
+});
