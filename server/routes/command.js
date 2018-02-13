bbde0b2d487f6fa7be32363f0fdc11b153726c67
Rémi Rigal <remi.rigal@ensta-bretagne.org>
2018-02-13 14:46:09 +0100

    TCP Command EOF

--- server/routes/command.js
+++ server/routes/command.js
@@ -4,24 +4,28 @@ var router = express.Router();
 
 
 router.post('/command/rtl', function(req, res) {
-    commandTCP.write('RTL');
+    commandTCP.write('RTL\0');
 });
 
 router.post('/command/stop', function(req, res) {
-    commandTCP.write('STOP');
+    commandTCP.write('STOP\0');
 });
 
 router.post('/command/start', function(req, res) {
-    commandTCP.write('START');
+    commandTCP.write('START\0');
 });
 
 router.post('/command/idle', function(req, res) {
-    commandTCP.write('IDLE');
+    commandTCP.write('IDLE\0');
+});
+
+router.post('/command/mission', function(req, res) {
+    commandTCP.write('MISSION\0');
 });
 
 router.post('/command/spd', function(req, res) {
     var speed = parseFloat(req.body.data.speed);
-    commandTCP.write('SPD/' + speed);
+    commandTCP.write('SPD/' + speed + '\0');
 });
 
 module.exports = router;
