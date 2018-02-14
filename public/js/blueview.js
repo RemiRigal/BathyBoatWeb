5283e8e8483d6abf653674e8d4fd6648cc3067b6
Rémi Rigal <remi.rigal@ensta-bretagne.org>
2018-02-14 15:02:03 +0100

    BlueView real time

--- public/js/blueview.js
+++ public/js/blueview.js
@@ -2,7 +2,7 @@ var blueviewImage;
 
 
 function refreshBlueview() {
-    blueviewImage.attr('src', 'images/blueview/img.png?' + new Date().getTime());
+    blueviewImage.attr('src', 'http://' + document.location.hostname + ':29201/images/blueview/imgn.png?' + new Date().getTime());
 }
 
 
--- server/utils/ppm_converter.js
+++ server/utils/ppm_converter.js
@@ -5,7 +5,8 @@ var PNG = require('pngjs').PNG;
 
 var blueviewImagePattern = '/images/blueview';
 var ppmPath = '/home/user/BlueView/bvtsdk/examples/net_sonar/cimg.ppm';
-var pngPath = __dirname + '/../../public/images/blueview/img.png';
+var pngPath = '/var/www/public/images/blueview/img.png';
+var pngNormalizedPath = '/var/www/public/images/blueview/imgn.png';
 
 
 module.exports = function(req, res, next) {
@@ -14,24 +15,34 @@ module.exports = function(req, res, next) {
         return;
     }
     shell.exec('convert ' + ppmPath + ' ' + pngPath);
-
-    fs.createReadStream(pngPath).pipe(new PNG({filterType: 4}))
-        .on('parsed', function() {
-            for (var y = 0; y < this.height; y++) {
-                for (var x = 0; x < this.width; x++) {
-                    var idx = (this.width * y + x) << 2;
-
-                    // invert color
-                    this.data[idx] = 255 - this.data[idx];
-                    this.data[idx+1] = 255 - this.data[idx+1];
-                    this.data[idx+2] = 255 - this.data[idx+2];
-
-                    // and reduce opacity
-                    this.data[idx+3] = this.data[idx+3] >> 1;
-                }
+    var readStream = fs.createReadStream(pngPath).pipe(new PNG({filterType: 4}));
+    readStream.on('parsed', function() {
+        var min = 255;
+        var max = 0;
+        for (var y = 0; y < this.height; y++) {
+            for (var x = 0; x < this.width; x++) {
+                var idx = (this.width * y + x) << 2;
+                min = Math.min(min, this.data[idx], this.data[idx+1], this.data[idx+2]);
+                max = Math.max(max, this.data[idx], this.data[idx+1], this.data[idx+2]);
             }
-            this.pack().pipe(fs.createWriteStream(pngPath));
-            console.log('Done');
+        }
+        var lerp = function(x) {
+            return Math.round(255 * (max - x) / (max - min));
+        };
+
+        for (var y = 0; y < this.height; y++) {
+            for (var x = 0; x < this.width; x++) {
+                var idx = (this.width * y + x) << 2;
+
+                this.data[idx] = 255 - lerp(this.data[idx]);
+                this.data[idx+1] = 255 - lerp(this.data[idx+1]);
+                this.data[idx+2] = 255 - lerp(this.data[idx+2]);
+            }
+        }
+        var writeStream = fs.createWriteStream(pngNormalizedPath);
+        this.pack().pipe(writeStream);
+        writeStream.on('close', function() {
             next();
         });
+    });
 };
