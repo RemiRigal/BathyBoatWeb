e5b5d4434f6ee2511248c8fa1689e3669d89f536
Rémi Rigal <remi.rigal@ensta-bretagne.org>
2018-03-04 16:17:40 +0100

    Unset minimap mission on import

--- public/js/imp_exp_mission.js
+++ public/js/imp_exp_mission.js
@@ -34,7 +34,7 @@ function importMissions(jsonMission) {
     missions.forEach(function(m) {
         m.htmlElement.remove();
         globalMap.unsetMission(m);
-        //miniMap.unsetMission(m);
+        miniMap.unsetMission(m);
     });
     missionList.html('');
     missionId = 0;
